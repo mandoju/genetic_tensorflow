f1a8f1a02cc4d8f8b189606c8ba8ff94a1113c1d
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2018-03-22 14:34:26 -0300

    paralelização do tensorflow...( preciso testar em casa)

--- teste_iris.py
+++ teste_iris.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from random import randrange
 import tensorflow as tf
 from multiprocessing.dummy import Pool as ThreadPool
-
+import time
 
 def input_fn(df, labels):
     feature_cols = {k: tf.constant(df[k].values, shape=[df[k].size, 1]) for k in columns}
@@ -31,12 +31,11 @@ def classifier_outputs(i):
 
     dnn.fit(input_fn=lambda: input_fn(X_train, y_train), steps=1000)
 
-    #dnn.evaluate(input_fn=lambda: input_fn(X_test, y_test), steps=1)
+    print("eu sou o " + str(i))
     return ObjetoResultado(dnn , dnn.evaluate)
 
-#print(check_output(["ls", "./input"]).decode("utf8"))
 
-iris = pd.read_csv(".\input\Iris.csv")
+iris = pd.read_csv("./input/Iris.csv")
 iris.head()
 
 print(iris.shape)
@@ -48,29 +47,22 @@ X_train, X_test, y_train, y_test = train_test_split(iris.iloc[:, 1:5], iris["Spe
 
 columns = iris.columns[1:5]
 
-# print(columns)
-# print(X_train.shape)
-# print(X_test.shape)
-# print(y_train.values)
-
 feature_columns = [tf.contrib.layers.real_valued_column(k) for k in columns]
 
 classifier = []
 ev = []
 inputs = [1,2,3,4]
 pool = ThreadPool(4)
+start = time.time()
 results = pool.map(classifier_outputs, inputs)
 pool.close()
 pool.join()
+end = time.time()
+print(end - start)
 
+for i in range(4):
 
-for i in range(5):
-
-    print(" o resultado foi: " + str(results[i].evaluation["accuracy"]))
-    #print("o valor " + str(i) + " tem a accuracy: " + str(ev[i]['accuracy']))
+    print(" o resultado foi: " + str(results[i].evaluation))
 
 print("máximo é:" + str(max(evaluation['accuracy'] for evaluation in ev)))
 
-    # print(pred)
-
-    # print(list(pred))
