373064cdc323a4a3110ba188e367f44e9419b48e
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2019-02-20 16:58:07 -0300

    fix

--- crossover.py
+++ crossover.py
@@ -162,21 +162,21 @@ def crossover_conv(best_conv,best_bias,convulations,bias,populationShape,populat
         finish_bias = []
         permutations = [[0,1],[0,2],[1,2],[0,3],[1,2],[1,3],[2,3]]
         for key in best_conv: 
-                population = tf.stack([best_conv[key][0],best_conv[key][1],best_conv[key][2]]),best_conv[key][3]])
+                population = tf.stack([best_conv[key][0],best_conv[key][1],best_conv[key][2],best_conv[key][3]])
                 for permutation in permutations:
                         father_tensor = best_conv[key][permutation[0]]
                         mother_tensor = best_conv[key][permutation[1]]
                         
-                        #old_population = tf.stack([father_tensor,mother_tensor])
+                        # old_population = tf.stack([father_tensor,mother_tensor])
                         new_population = tf.map_fn(lambda x: generate_child_by_all(mother_tensor,father_tensor,mutationRate),tf.range( (population_size - size_neural_networks) //  len(permutations) ) ,dtype=tf.float32)
-                        print((population_size - size_neural_networks) //  len(permutations))
+                        # print((population_size - size_neural_networks) //  len(permutations))
                         population = tf.concat([population,new_population],0)
                 #print(dir(tf))
                 #finish = tf.assign(best_conv[key], tf.stack(new_population))
                 #import ipdb; ipdb.set_trace();
                 finish_conv.append(tf.assign(convulations[key], tf.stack(population)))
         for key in best_bias: 
-                population = tf.stack([best_bias[key][0],best_bias[key][1],best_bias[key][2]]),best_bias[key][3]])
+                population = tf.stack([best_bias[key][0],best_bias[key][1],best_bias[key][2],best_bias[key][3]])
                 for permutation in permutations:
 
                         father_tensor = best_bias[key][permutation[0]]
