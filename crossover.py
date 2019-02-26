d097e8c6dd48ec0d8d31b8d58d3a2a9348386384
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2019-02-26 09:52:23 -0300

    fixing mutation population size

--- crossover.py
+++ crossover.py
@@ -219,7 +219,7 @@ def crossover_conv(best_conv,best_bias,convulations,bias,populationShape,populat
         finish_bias = []
         tamanhoElite = population_size // 10
         tamanhoCrossover = tamanhoElite
-        tamanhoMutacoes = population_size - (tamanhoElite + tamanhoCrossover) // 3
+        tamanhoMutacoes = (population_size - (tamanhoElite + tamanhoCrossover) )// 3
         tamanhoUltimaMutacao = population_size - (tamanhoElite + tamanhoCrossover + tamanhoMutacoes * 2)
         print(tamanhoUltimaMutacao)
         permutations = tf.range(tamanhoElite)
