1d7b5449209e5f318e5557dca0f47865ac6981ae
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2019-02-26 11:36:19 -0300

    using square mean error

--- genetic_class.py
+++ genetic_class.py
@@ -37,7 +37,7 @@ class Population:
             inverted_sqe = -self.neural_networks.square_mean_error #tf.multiply(self.neural_networks.square_mean_error , tf.constant(-0.1),name="inverted_sqe")
             #fitness = self.neural_networks.accuracies * 100  + inverted_sqe + inverted_cost
             #fitness = self.neural_networks.accuracies
-            fitness = inverted_cost / 2  + inverted_sqe / 2
+            fitness =  inverted_sqe / 2
             best_conv, best_bias, the_best_conv, the_best_bias, mutate_conv, mutate_bias = choose_best_tensor_conv(self.neural_networks.convulations, self.neural_networks.biases, fitness, self.populationSize // 10)
             # self.neural_networks.best_conv = the_best_conv
             # self.neural_networks.best_bias = the_best_bias
