a1b65c0e2cf8dd34f809aba2a742e4fecb879c08
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2019-02-27 09:22:25 -0300

    Fix na mutação e aumento da população

--- mutation.py
+++ mutation.py
@@ -26,7 +26,7 @@ def mutation(tensor, mutationRate,mutationTax):
 
         random_mutation = tf.multiply(random_array_binary,random_array_values)
         
-        mutated = tensor + random_mutation
+        mutated = tf.multiply(tensor,random_mutation)
         #comparison = tf.math.equal( random_mutation, tf.constant( 0.0 ) )
         #mutated = tf.where(comparison, tensor, random_mutation)
         return mutated
