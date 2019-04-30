f232f3beab1c797ef21e75089ddce7d3dfcb8981
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2019-04-30 08:11:45 -0300

    fixing execution to 10 layers

--- run_multiple_python_test.bash
+++ run_multiple_python_test.bash
@@ -10,8 +10,8 @@ python test.py 40 5
 python test.py 80 5
 python test.py 160 5
 python gradient.py 5
-python test.py 20 10
-python test.py 40 10
-python test.py 80 10
-python test.py 160 10
-python gradient.py 10
+python test.py 20 
+python test.py 40 
+python test.py 80 
+python test.py 160 
+python gradient.py 
