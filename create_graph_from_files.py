21f0981ab5990e397c5b524df42c2f80d4d9f5eb
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2019-05-01 20:46:36 -0300

    Initializer genetic

--- create_graph_from_files.py
+++ create_graph_from_files.py
@@ -8,11 +8,10 @@ import pickle
 #files_pickle = ['./graphs/20_he_menor.pckl','./graphs/40_he_menor.pckl','./graphs/80_he_menor.pckl','./graphs/160_he_menor.pckl','./graphs/gradient.pckl']
 #files_pickle = ['./graphs/20_3.pckl','./graphs/40_3.pckl','./graphs/80_3.pckl','./graphs/160_3.pckl','./graphs/gradient_3.pckl']
 #files_pickle = ['./graphs/20_5.pckl','./graphs/40_5.pckl','./graphs/80_5.pckl','./graphs/160_5.pckl','./graphs/gradient_5.pckl']
-files_pickle = ['./graphs/20_10.pckl','./graphs/40_10.pckl','./graphs/80_10.pckl','./graphs/160_10.pckl','./graphs/gradient_10.pckl']
+#files_pickle = ['./graphs/20_10.pckl','./graphs/40_10.pckl','./graphs/80_10.pckl','./graphs/160_10.pckl','./graphs/gradient_10.pckl']
 #files_pickle = ['./graphs/20_he_menor.pckl','./graphs/40_he_menor.pckl','./graphs/80_he_menor.pckl','./graphs/160_he_menor.pckl','./graphs/gradient.pckl']
 
-
-graphs = []
+""" graphs = []
 
 for file_pickle in files_pickle:
     file_pickle_opened = open(file_pickle, 'rb') 
@@ -37,3 +36,32 @@ plt.show()
 
 
 
+ """
+#files_pickle = ['./graphs/20_he_menor.pckl','./graphs/40_he_menor.pckl','./graphs/80_he_menor.pckl','./graphs/160_he_menor.pckl','./graphs/gradient.pckl']
+files_pickle = ['./graphs/40_3.pckl','./graphs/gradient_3.pckl']
+
+graphs = []
+for file_pickle in files_pickle:
+    file_pickle_opened = open(file_pickle, 'rb') 
+    graphs.append(pickle.load(file_pickle_opened) )
+    file_pickle_opened.close();
+ 
+for graph in graphs:
+    if(graph.performance[0] < 0):
+        graph.performance = [x * -1 for x in graph.performance]
+
+print(graphs[0].performance)
+
+graph = plt.plot(graphs[0].tempo,graphs[0].performance , '-', label="Mutation_20")
+gradient = plt.plot(graphs[1].tempo,graphs[1].performance , '-', label="Mutation_20")
+
+#mutation_20 = plt.plot(graphs[0].tempo,graphs[0].performance , '-', label="Mutation_20")
+#mutation_40 = plt.plot(graphs[1].tempo,graphs[1].performance , '-', label="Mutation_40")
+#mutation_80 = plt.plot(graphs[2].tempo,graphs[2].performance , '-', label="Mutation_80")
+#mutation_160 = plt.plot(graphs[3].tempo,graphs[3].performance , '-', label="Mutation_160")
+#gradient = plt.plot(graphs[4].tempo,graphs[4].performance , '-', label="Gradient")
+#plt.legend([mutation_20,mutation_40,mutation_80,mutation_160,gradient],['mutação 20','mutação 40','mutação 80','mutação 160'])
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+plt.grid(True);
+plt.show()
--- genetic_class.py
+++ genetic_class.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from neural_network import Neural_network
 from create_population import create_population
 from choose_best import choose_best
@@ -14,8 +15,6 @@ import pickle
 import sys
 matplotlib.use('Agg')
 
-import matplotlib.pyplot as plt
-
 
 class Population:
 
@@ -23,46 +22,55 @@ class Population:
         self.populationSize = geneticSettings['populationSize']
         self.layers = geneticSettings['layers']
         self.mutationRate = geneticSettings['mutationRate']
-        self.population, self.populationShape, self.convulations, self.bias = create_population(geneticSettings['layers'], geneticSettings['weights_convulation'],geneticSettings['biases'],geneticSettings['populationSize'])
+        self.population, self.populationShape, self.convulations, self.bias = create_population(
+            geneticSettings['layers'], geneticSettings['weights_convulation'], geneticSettings['biases'], geneticSettings['populationSize'])
         self.neural_networks = Neural_network(
-           geneticSettings['populationSize'] , geneticSettings['layers'], self.convulations,self.bias, './log/')
+            geneticSettings['populationSize'], geneticSettings['layers'], self.convulations, self.bias, './log/')
         self.geneticSettings = geneticSettings
         self.current_epoch = 0
         self.eliteSize = int(geneticSettings['elite'] * self.populationSize)
-        self.slice_sizes = [self.populationSize * x for x in geneticSettings['genetic_operators_size'] ]
+        self.slice_sizes = [self.populationSize *
+                            x for x in geneticSettings['genetic_operators_size']]
         self.genetic_operators_size = geneticSettings['genetic_operators_size']
-        self.fineTuningRate = geneticSettings['fineTuningRate'] 
+        self.fineTuningRate = geneticSettings['fineTuningRate']
         self.fineTuningBoolean = geneticSettings['fineTuning']
-        #self.slice_sizes.append(self.eliteSize)
+        # self.slice_sizes.append(self.eliteSize)
+
     def run_epoch(self):
 
-        #print("neural networks fitness run:")
-        #fitness_operator = tf.placeholder(tf.int16)
+        # print("neural networks fitness run:")
+        # fitness_operator = tf.placeholder(tf.int16)
         start = time.time()
-        
+
         self.neural_networks.run()
-        
-        self.mutationRate = tf.placeholder(tf.float32,shape=[])
-        self.operatorSize = tf.placeholder(tf.float32,shape=[len(self.geneticSettings['genetic_operators_size'])])
+
+        self.mutationRate = tf.placeholder(tf.float32, shape=[])
+        self.operatorSize = tf.placeholder(
+            tf.float32, shape=[len(self.geneticSettings['genetic_operators_size'])])
         if(self.geneticSettings['fitness'] == 'cross_entropy'):
             fitness = -self.neural_networks.cost
         elif(self.geneticSettings['fitness'] == 'square_mean_error'):
-            fitness = -self.neural_networks.square_mean_error 
+            fitness = -self.neural_networks.square_mean_error
         elif(self.geneticSettings['fitness'] == 'root_square_mean_error'):
             fitness = -self.neural_networks.root_square_mean_error
         elif(self.geneticSettings['fitness'] == 'cross_entropy_mix_accuracies'):
-            fitness = self.neural_networks.accuracies  + self.neural_networks.cost
+            fitness = self.neural_networks.accuracies + self.neural_networks.cost
         else:
             fitness = self.neural_networks.accuracies
-        
-        best_conv, best_bias, the_best_conv, the_best_bias, mutate_conv, mutate_bias = choose_best(self.geneticSettings['selection'],self.neural_networks.convulations, self.neural_networks.biases, fitness, self.eliteSize)
-        
-        finish_conv, finish_bias = apply_genetic_operatos(self.geneticSettings['genetic_operators'],self.operatorSize,self.eliteSize,self.convulations,self.bias, best_conv, best_bias, self.populationShape , self.populationSize, self.mutationRate,2,len(self.layers))
-            
+
+        best_conv, best_bias, the_best_conv, the_best_bias, mutate_conv, mutate_bias = choose_best(
+            self.geneticSettings['selection'], self.neural_networks.convulations, self.neural_networks.biases, fitness, self.eliteSize)
+
+        finish_conv, finish_bias = apply_genetic_operatos(self.geneticSettings['genetic_operators'], self.operatorSize, self.eliteSize,
+                                                          self.convulations, self.bias, best_conv, best_bias, self.populationShape, self.populationSize, self.mutationRate, 2, len(self.layers))
+
         merged = tf.summary.merge_all()
 
         self.current_epoch += 1
-       
+
+        optimizer = tf.train.AdamOptimizer(
+            learning_rate=0.0001).minimize(self.neural_networks.cost[0])
+
         sess = tf.Session()
         # writer = tf.summary.FileWriter(self.neural_networks.logdir, sess.graph)
         # run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
@@ -77,13 +85,13 @@ class Population:
         print("local variables:", time.time() - start)
 
         pop_array = []
-        
+
         finished_array = []
-        
+
         train_x = self.neural_networks.train_x
-        #test_x = self.neural_networks.test_x
+        # test_x = self.neural_networks.test_x
         train_y = self.neural_networks.train_y
-        #test_y = self.neural_networks.test_y
+        # test_y = self.neural_networks.test_y
         # print(len(train_x))
         start_time = time.time()
         acuracias = []
@@ -94,83 +102,116 @@ class Population:
         print(mutate)
         last_cost = 0
         for i in range(self.geneticSettings['epochs']):
-            
+
             print("época: " + str(i))
             start_generation = time.time()
 
             batch_size = 128
+            if(i == 0):
+                for batch in range(len(train_x)//batch_size):
+                        # for j in range(self.geneticSettings['inner_loop']):
+                    print("batch: " + str(batch))
+                    start_batch = time.time()
+                    batch_x = train_x[batch *
+                                      batch_size:min((batch+1)*batch_size, len(train_x))]
+                    batch_y = train_y[batch *
+                                      batch_size:min((batch+1)*batch_size, len(train_y))]
 
-            for batch in range(len(train_x)//batch_size):
-                    #for j in range(self.geneticSettings['inner_loop']):
-                        print("batch: " + str(batch))
-                        start_batch = time.time()
-                        batch_x = train_x[batch*batch_size:min((batch+1)*batch_size,len(train_x))]
-                        batch_y = train_y[batch*batch_size:min((batch+1)*batch_size,len(train_y))]  
+                    print("Mutação atual: " + str(mutate))
+                    print(self.slice_sizes)
 
-                        print("Mutação atual: " + str(mutate) )
-                        print(self.slice_sizes)
+                    run_options = tf.RunOptions(
+                        report_tensor_allocations_upon_oom=True)
 
-                        run_options = tf.RunOptions(report_tensor_allocations_upon_oom = True)
+                    predicts, label_argmax, accuracies, cost, finished_conv, finished_bias = sess.run([self.neural_networks.argmax_predicts, self.neural_networks.label_argmax, self.neural_networks.accuracies, fitness, finish_conv, finish_bias], feed_dict={
+                        self.neural_networks.X: batch_x, self.neural_networks.Y: batch_y, self.mutationRate: mutate, self.operatorSize: self.slice_sizes}, options=run_options)
+                    msg = "Batch: " + str(batch)
+                    np.savetxt('predicts_save.txt', predicts)
+                    np.savetxt('Y.txt', label_argmax)
+                    print("Accuracy: ")
+                    print(accuracies)
+                    print("Cost: ")
+                    print(cost)
+                    print("tempo atual: " + str(time.time() - start_time))
+                    # if(max(cost) < 3):
+                    acuracias.append(max(cost))
+                    tempos.append(time.time() - start_time)
+                    if(self.fineTuningBoolean):
+                        # if(max(accuracies) <= last_accuracy):
+                        #     mutate += 0.1
+                        #     if(mutate > 0.7):
+                        #         mutate = 0.7
+                        # else:
+                        #     mutate -= 0.1
+                        #     if(mutate < 0.1):
+                        #         mutate = 0.1
+                        last_cost = max(cost)
 
-                        predicts,label_argmax,accuracies,cost,finished_conv,finished_bias = sess.run([self.neural_networks.argmax_predicts,self.neural_networks.label_argmax,self.neural_networks.accuracies,fitness,finish_conv,finish_bias], feed_dict={
-                            self.neural_networks.X: batch_x, self.neural_networks.Y: batch_y, self.mutationRate: mutate, self.operatorSize: self.slice_sizes}, options=run_options )
-                        msg = "Batch: " + str(batch)
-                        np.savetxt('predicts_save.txt',predicts)
-                        np.savetxt('Y.txt',label_argmax)
-                        print("Accuracy: ")
-                        print(accuracies)
-                        print("Cost: ")
-                        print(cost)
-                        print("tempo atual: " + str(time.time() - start_time))
-                        # if(max(cost) < 3):
-                        acuracias.append(max(cost))
-                        tempos.append(time.time() - start_time)
-                        if(self.fineTuningBoolean):
-                            # if(max(accuracies) <= last_accuracy):
-                            #     mutate += 0.1
-                            #     if(mutate > 0.7):
-                            #         mutate = 0.7
-                            # else:
-                            #     mutate -= 0.1
-                            #     if(mutate < 0.1):
-                            #         mutate = 0.1
-                            last_cost = max(cost)
+                        last_population_slice = 0
+                        operators_max = []
+                        for population_slice in self.slice_sizes:
+                            slice_finish = int(
+                                last_population_slice+population_slice-1)
+                            if(population_slice > 1):
+                                operators_max.append(
+                                    max(cost[last_population_slice:slice_finish]))
+                            else:
+                                operators_max.append(
+                                    cost[last_population_slice])
+                            last_population_slice += int(population_slice)
 
-                            last_population_slice = 0
-                            operators_max = []
-                            for population_slice in self.slice_sizes:
-                                slice_finish = int(last_population_slice+population_slice-1)
-                                if(population_slice > 1):
-                                    operators_max.append(max(cost[last_population_slice:slice_finish]))
-                                else:
-                                    operators_max.append(cost[last_population_slice])
-                                last_population_slice += int(population_slice)
-                            
-                            max_fitness_operator_index = operators_max.index(max(operators_max))
-                            
-                            #possible_slices_remove = self.slice_sizes
-                            #minimum_not_one = possible_slices[operators_max.index(min(operators_max))]
-                            slice_with_operator = np.column_stack((self.slice_sizes,operators_max,range(len(self.slice_sizes))))
-                            slice_with_operator = list(filter(lambda x: x[0] > 1,slice_with_operator))
-                            print(slice_with_operator)
-                            min_fitness_slice = min(slice_with_operator,key=lambda x: x[1])
-                            print(min_fitness_slice)
-                            min_fitness_operator_index = int(min_fitness_slice[2])
-                            print(min_fitness_operator_index)
-                            if(self.slice_sizes[min_fitness_operator_index] > 1):
-                                self.slice_sizes[max_fitness_operator_index] += 1
-                                self.slice_sizes[min_fitness_operator_index] -= 1
+                        max_fitness_operator_index = operators_max.index(
+                            max(operators_max))
+
+                        # possible_slices_remove = self.slice_sizes
+                        # minimum_not_one = possible_slices[operators_max.index(min(operators_max))]
+                        slice_with_operator = np.column_stack(
+                            (self.slice_sizes, operators_max, range(len(self.slice_sizes))))
+                        slice_with_operator = list(
+                            filter(lambda x: x[0] > 1, slice_with_operator))
+                        print(slice_with_operator)
+                        min_fitness_slice = min(
+                            slice_with_operator, key=lambda x: x[1])
+                        print(min_fitness_slice)
+                        min_fitness_operator_index = int(min_fitness_slice[2])
+                        print(min_fitness_operator_index)
+                        if(self.slice_sizes[min_fitness_operator_index] > 1):
+                            self.slice_sizes[max_fitness_operator_index] += 1
+                            self.slice_sizes[min_fitness_operator_index] -= 1
+            else:
+                    for batch in range(len(train_x)//batch_size):
+                        batch_x = train_x[batch *
+                                          batch_size:min((batch+1)*batch_size, len(train_x))]
+                        batch_y = train_y[batch *
+                                          batch_size:min((batch+1)*batch_size, len(train_y))]
+                        # Run optimization op (backprop).
+                    # Calculate batch loss and accuracy
+                        opt = sess.run(optimizer, feed_dict={self.neural_networks.X: batch_x, self.neural_networks.Y: batch_y,
+                                       self.mutationRate: mutate, self.operatorSize: self.slice_sizes}, options=run_options)
+                        loss = sess.run(cost, feed_dict={self.neural_networks.X: batch_x, self.neural_networks.Y: batch_y, self.mutationRate: mutate, self.operatorSize: self.slice_sizes}, options=run_options)
+                    print("Iter " + str(i) + ", Loss= " +
+                          "{:.6f}".format(loss) )
+                    print("Optimization Finished!")
+
+                    # Calculate accuracy for all 10000 mnist test images
+                    # test_acc,valid_loss = sess.run([accuracy,cost], feed_dict={
+                    #           self.neural_networks.X: batch_x, self.neural_networks.Y: batch_y, self.mutationRate: mutate, self.operatorSize: self.slice_sizes}, options=run_options ))
+                    acuracias.append(loss)
+                    tempos.append(time.time() - start_time)
+
             # mutate = mutate * 2
         sess.close()
-        file_string = []
+        file_string=[]
         if(len(sys.argv) > 2):
-          file_string = './graphs/' + str(self.populationSize)  + '_' +  sys.argv[2] +   '.pckl'
+            file_string='./graphs/' + \
+                str(self.populationSize) + '_' + sys.argv[2] + '.pckl'
         else:
-          file_string = './graphs/' + str(self.populationSize)  + '_10.pckl'
+            file_string='./graphs/' + str(self.populationSize) + '_10.pckl'
         with open(file_string, 'wb') as save_graph_file:
-            save_graph = Graph(tempos,acuracias)
-            pickle.dump(save_graph,save_graph_file)
-            print('salvei em: ' + '.\graphs\\' + str(self.populationSize) + '.pckl')
+            save_graph=Graph(tempos, acuracias)
+            pickle.dump(save_graph, save_graph_file)
+            print('salvei em: ' + '.\graphs\\' +
+                  str(self.populationSize) + '.pckl')
         plt.plot(tempos, acuracias, '-', lw=2)
         plt.grid(True)
         plt.savefig('acuracias.png')
