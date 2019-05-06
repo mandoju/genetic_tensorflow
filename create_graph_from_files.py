1e0da51d44899271193098e4755c7dc678e9f4ce
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2019-05-06 13:57:33 -0300

    adição do gráfico do fine_tuning

--- create_graph_from_files.py
+++ create_graph_from_files.py
@@ -99,6 +99,20 @@ plt.grid(True)
 plt.ylim(0,1)
 plt.show()
 
+for graph_file in graphs:
+    graph = graph_file.graph
+    if(graph.performance[0] < 0):
+        graph.performance = [x * -1 for x in graph.performance]
+    if(len(graph.fine_tuning) > 0):
+        plt.plot(graph.tempo,graph.accuracy , '-', label=graph_file.name)
+plt.legend(loc='upper right')
+plt.title("Fine_Tuning x Tempo do conjunto de treino")
+plt.xlabel("Tempo (s)")
+plt.ylabel("População")
+plt.grid(True)
+plt.ylim(0,1)
+plt.show()
+
 """ 
 for graph_file in graphs:
     graph = graph_file.graph
